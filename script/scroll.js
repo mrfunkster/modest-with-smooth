9ef09d8a2fdd5875ed8f3b1a0418b45f78e100d7
mr.funkster <mr.funksters@gmail.com>
2020-06-16 14:28:00 +0300

    adding a jQuery smooth scroll

--- script/scroll.js
+++ script/scroll.js
@@ -12,16 +12,28 @@ for (let anchor of anchors) {
     } else {
         headerOffset = 80;
     }
+
+        // var target = $(this).attr('href');
+        // $('html, body').animate({
+        //     scrollTop: $(target).offset().top - headerOffset
+        // }, 800);
+
+    // const blockID = anchor.getAttribute('href').substr(1);
+    // let block = document.getElementById(blockID);
+    // let blockPosition = block.offsetTop;
+    // let offsetPosition = blockPosition - headerOffset;
+
+    //     window.scrollTo({
+    //         top: offsetPosition,
+    //         behavior: "smooth"
+    //     });
     
     const blockID = anchor.getAttribute('href').substr(1);
     let block = document.getElementById(blockID);
-    let blockPosition = block.offsetTop;
-    let offsetPosition = blockPosition - headerOffset;
+    $('html, body').animate({
+        scrollTop: $(block).offset().top - headerOffset
+    }, 800);
 
-        window.scrollTo({
-            top: offsetPosition,
-            behavior: "smooth"
-        });
     })
 }
 
